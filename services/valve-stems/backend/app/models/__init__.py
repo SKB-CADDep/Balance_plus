25e828d16392be15af979ce0da8f603ecbf0e707
Lev Shlyogin <147638117+LevShlyogin@users.noreply.github.com>
2026-02-18 17:35:38 +0500

    Update __init__.py

--- services/valve-stems/backend/app/models/__init__.py
+++ services/valve-stems/backend/app/models/__init__.py
@@ -1,12 +1,10 @@
 from app.models.calculation_result import CalculationResultDB
 from app.models.turbine import Turbine
 from app.models.valve import Valve
-from .project import Project
 
 
 __all__ = [
     "CalculationResultDB",
     "Turbine",
     "Valve",
-    "Project"
 ]
