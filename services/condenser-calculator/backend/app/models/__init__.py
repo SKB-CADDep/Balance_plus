29d2ae1dbeb29d953531c48729bb6e9c4f4d01dd
Tr1pa <emdan@bk.ru>
2026-04-14 11:06:17 +0300

    fix conflicts

--- services/condenser-calculator/backend/app/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from .condenser import Condenser
-from .material import Material
-from .calculation_result import CalculationResult
-
-__all__ = [
-    "Condenser",
-    "Material",
-    "CalculationResult",
-]
